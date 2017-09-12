0d3cb50b4efaf1bbaf65419acd01b23721424d43
hugo araujo de sousa <hugosousa@velhas.grad.dcc.ufmg.br>
2017-09-12 11:33:06 -0300

    tp1: add input files processing

--- tp1/tp1.py
+++ tp1/tp1.py
@@ -7,9 +7,16 @@ tp1.py
 '''
 
 import argparse
+import csv
 
 # Adds optional command line arguments to the program.
 parser = argparse.ArgumentParser()
+
+parser.add_argument('TRAIN_FILE', metavar='train', type=str,
+	help='Name of training data file')
+parser.add_argument('TEST_FILE', metavar='test', type=str,
+	help='Name of test data file')
+
 parser.add_argument('-d', dest='MAX_DEPTH', default=7, type=int,
 	help='Maximum depth of the individual trees')
 parser.add_argument('-s', dest='RSEED', default=0, type=int,
@@ -19,16 +26,42 @@ parser.add_argument('-p', dest='POP_SIZE', default=0, type=int,
 
 args = parser.parse_args()
 
-# Random numbers generation setup. '''
+# Random numbers generation setup.
 import random
 random.seed(args.RSEED)
 
 import individual as ind
 
 
+################################################################################
+
+
+def list_from_csv(filename):
+	''' Gets a list of lists from a CSV file.
+		@filename: CSV file name;
+		@return: List of lists with the file's content. '''
+	file = open(filename, 'r')
+	reader = csv.reader(file)
+	csv_list = list(reader)
+	file.close()
+
+	return csv_list
+
+
 def main():
-	a = ind.Individual.full(args.MAX_DEPTH, 5)
-	print(a)
+	''' Main function. '''
+
+	# Obtains the input data.
+	train_data = list_from_csv(args.TRAIN_FILE)
+	test_data = list_from_csv(args.TEST_FILE)
+
+	num_var = len(train_data[0]) - 1
+
+	population = ind.Individual.ramped_half(args.POP_SIZE, args.MAX_DEPTH, \
+		num_var)
+
+	for individual in population:
+		print(individual)
 
 
 main()
